27c0d353e3b5ba3d57a963d1fbacfafcd08ef2e2
onechance <zhouh08@hotmail.com>
2014-06-21 10:36:17 +0800

    1

---
+++ Assets/my_wow/scripts/login/login.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class login : MonoBehaviour
+{
+
+		public UILabel acc_label;
+		public UILabel pwd_label;
+
+		void OnClick ()
+		{
+				if (acc_label.text != string.Empty && pwd_label.text != string.Empty) {
+						Debug.Log ("send message to server");		
+				} else {
+						Debug.Log ("login error");		
+				}
+						
+		}
+}
