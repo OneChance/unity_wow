f830833d3413036d21a0621e17db15205e9e2260
onechance <zhouh08@hotmail.com>
2014-06-22 10:59:28 +0800

    0

---
+++ Assets/my_wow/scripts/network/NetWorkScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+
+public class NetWorkScript
+{
+		private static NetWorkScript instance;
+		private Socket socket;
+		private string ip = "127.0.0.1";
+		private int port = 10100;
+
+		public static NetWorkScript getInstance ()
+		{
+				if (instance == null) {
+						instance = new NetWorkScript ();		
+				}
+				return instance;
+		}
+
+		public void init ()
+		{
+				socket = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				socket.Connect (ip, port);
+		}
+}
