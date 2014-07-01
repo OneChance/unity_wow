bceddf480734758a16d5661c8f9bff9d93ca4ff2
onechance <zhouh08@hotmail.com>
2014-07-01 13:22:08 +0800

    d

--- Assets/my_wow/scripts/login/LoginController.cs
+++ Assets/my_wow/scripts/login/LoginController.cs
@@ -6,7 +6,7 @@ public class LoginController: MonoBehaviour
 		public void onMessage (SocketModel module)
 		{
 				Account account = JsonUtil<Account>.decode (module.message);
-
+				
 				if (account.userid != 0) {
 						Debug.Log ("login ok!");
 				} else {
